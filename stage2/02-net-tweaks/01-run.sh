6dfce541e920a308a8a976b2d6cb546552036e8d
Kevin Van Wilder <kevin@van-wilder.be>
2018-08-16 23:56:13 +0200

    Autoconnect to local network

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -6,3 +6,11 @@ install -v -m 644 files/wait.conf		"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.serv
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
+echo "
+network={
+    ssid="${NETWORK_NAME}"
+    psk="${NETWORK_PSK}"
+    key_mgmt=WPA-PSK
+}
+
+" >> ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
